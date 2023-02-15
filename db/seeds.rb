3b1b4ed7fc1e4361d93c75f53d6b08e75f58373d
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-15 14:07:38 +1100

    fun controversial questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,16 @@ Question.create(content: 'Who is the artist behind "My Beautiful Dark Twisted Fa
 Question.create(content: 'What is the main theme of "My Beautiful Dark Twisted Fantasy"?', option1:'Ego and excess', option2:'Heartbreak and regret', option3:'Political activism', option4:'Love and relationships', correct_answer:'Ego and excess', category:'music')
 Question.create(content: 'In what year was "My Beautiful Dark Twisted Fantasy" released?', option1:'2010', option2:'2011', option3:'2009', option4:'2008', correct_answer:'2010', category:'music')
 
+# Random controversy 
+Question.create(content: 'What is the best solution to control the human population?', option1:'Family planning', option2:'War', option3:'Euthanasia', option4:'Abortion', correct_answer:'Family planning', category:'controversy')
+Question.create(content: 'When should someone be considered human?', option1:'At conception', option2:'At birth', option3:'When they reach adulthood', option4:'When they have self-awareness', correct_answer:'At birth', category:'controversy')
+Question.create(content: 'Was Hitler good for the German economy?', option1:'Yes', option2:'No', option3:'Undecided', option4:'It depends', correct_answer:'No', category:'controversy')
+Question.create(content: 'Is it better to stay at home and learn or risk getting COVID while studying in person?', option1:'Stay at home', option2:'Study in person', option3:'It depends', option4:'Undecided', correct_answer:'Stay at home', category:'controversy')
+Question.create(content: 'Is Hong Kong part of China?', option1:'Yes', option2:'No', option3:'Undecided', option4:'It depends', correct_answer:'Yes', category:'controversy')
+Question.create(content: 'Was Poland part of Nazi Germany in 1940?', option1:'Yes', option2:'No', option3:'Undecided', option4:'It depends', correct_answer:'Yes', category:'history')
+
+
+
 # Random categories
 Question.create(content: 'This author lives in a castle and is transphobic?', option1:'Dr. Seuss', option2:'Charles Dickens', option3:'JK Rowling', option4:'Roalad Dahl', correct_answer:'JK Rowling', category:'authors')
 
