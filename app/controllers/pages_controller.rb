a9584c820af888cdb9a084f861a35f6ba30bd23a
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-16 15:53:14 +1100

    pre deployment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
     # Since the backend was created in API mode, html can not be rendered without adding the middleware ActionDispatch::static
     def index
-        render plain: "Please visit localhost:3001 for frontend"
+        render plain: "Visit https://bit.ly/bkquizserver for our backend git repo, https://bit.ly/fnquizclient for frontend repo or visit the live demo at quizga.com"
     end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins '*' # this needs to be an explicit list of URLS for Heroku
+        origins '61.69.254.170', 'https://mighty-eyrie-05838.herokuapp.com/'# this needs to be an explicit list of URLS for Heroku
         resource '*',
             :headers => :any,
             :methods => %i( get post put patch delete options head )
