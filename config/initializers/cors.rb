9d185809efa574229c4de74a4aa5fa63e3e0a186
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-16 23:20:00 +1100

    changes cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins '61.69.254.170', 'https://mighty-eyrie-05838.herokuapp.com/'# this needs to be an explicit list of URLS for Heroku
+        origins '61.69.254.170', 'https://mighty-eyrie-05838.herokuapp.com/', 'https://roaring-lily-0e4daf.netlify.app/', 'quizga.com', 'localhost', '159.196.113.253'
         resource '*',
-            :headers => :any,
-            :methods => %i( get post put patch delete options head )
+            headers: :any,
+            methods: %i(get post put patch delete options head)
     end
 end
